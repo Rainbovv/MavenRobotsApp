e62656554eb2827bb5f5e1d56bd137027307f44f
Rainbovv <turcanu.radu17@gmail.com>
2020-08-02 05:35:37 +0300

    Added: ChargeTest.java Edited: pom.xml

---
+++ src/test/java/ChargeTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+import robo.BetaRobot;
+import robo.RobotFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChargeTest {
+
+	private BetaRobot robot = (BetaRobot)RobotFactory.getInstance().getRobot("Beta", "B123");
+
+	@Test
+	public void negativeChargeTest() {
+		robot.setCharge(-100);
+		assertFalse(robot.getCharge() < 0,"Charge test failed" +
+														   "\nREASON: Values les than 0 not allowed!");
+	}
+
+	@Test
+	public void chargeOverflowTest(){
+		robot.setCharge(1000);
+		assertFalse(robot.getCharge() > 100,"Charge test failed" +
+															 "\nREASON: Values more than 0 not allowed!");
+	}
+
+	@Test
+	public void batteryDischargingInMoving() {
+		robot.setCharge(50);
+		while(robot.moveRight());
+		assertFalse(robot.getCharge() != 4 ||
+							 robot.getX() != 92,"Charge test failed" +
+													    "\nREASON: The moving system should be calibrated!" ) ;
+	}
+}
